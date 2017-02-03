ec997c9ca8d0a7b61f9fe913e70d1a7a5119632c
Prakash Rana <prakashrana9847067614@gmail.com>
2017-02-04 01:23:45 +0530

    modals added

---
+++ modals.py
@@ -0,0 +1,22 @@
+
+from google.appengine.ext import db
+# database for users
+class Users(db.Model):
+    user_name = db.StringProperty(required = True)
+    user_password = db.StringProperty(required = True)
+    user_email = db.StringProperty()
+
+
+# Database for Likes
+class Likes(db.Model):
+    post_id = db.StringProperty(required = True)
+    username = db.StringProperty(required = True)
+
+
+# Database for comments
+class Comments(db.Model):
+    username = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    post_id = db.StringProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
